32d4b03276df2815ce51d54a2cff95f1c9735879
SafieDeme9 <safiedeme9@gmail.com>
2023-03-29 06:39:51 -0400

    task 2 1

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 #Puppet manifest that kills a process named killmenow
 
 exec { 'killmenow'
-  command  => '/usr/bin/pkill killmenow',
+  command  => '/usr/bin/pkill -9 killmenow',
   onlyif   => 'pgrep killmenow',
   provider => 'shell',
 }
