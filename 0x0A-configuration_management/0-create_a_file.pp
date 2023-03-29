2b2c928f549d7153238de8bdb5c7dbfa59d76ab6
SafieDeme9 <safiedeme9@gmail.com>
2023-03-29 06:03:04 -0400

    task 0 corrected

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 #Create a file with puppet
 
 file { '/tmp/school':
-  ensure => present,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0744',
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
 
